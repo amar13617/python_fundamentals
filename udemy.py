4f3c8b140201f14943058dfceadc59791d41342e
LENOVO <amar13617@gmail.com>
2023-06-05 14:04:30 +0530

    if else committed

--- udemy.py
+++ udemy.py
@@ -1,1 +1,8 @@
-print("hello")
+friend = "Rolf"
+user_name = input("Enter your name:")
+
+if user_name == friend:
+    print("Hello, friend")
+    print("This runs too")
+
+print("This runs anyway")
