42c77f95bd0df5a7b916a63cc3c6ce92b8a7ed8a
LENOVO <amar13617@gmail.com>
2023-06-05 14:22:13 +0530

    if else bool committed

--- if else.py
+++ if else.py
@@ -1,7 +1,12 @@
-friend = "Rolf"
-user_name = input("Enter your name:")
+#friend = "Rolf"
+#user_name = input("Enter your name:")
 
-if user_name == friend:
-    print("Hello, friend")
-else:
-    print("Hello, stranger")
+#if user_name == friend:
+#    print("Hello, friend")
+#else:
+#    print("Hello, stranger")
+    
+name = input("Enter name")
+print(bool(name))
+if name:
+    print("I know")
