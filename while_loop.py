36932eede4663bd678bf1618fa3dc549a1d12e3f
LENOVO <amar13617@gmail.com>
2023-06-07 11:01:06 +0530

    while loop commited in python

---
+++ while_loop.py
@@ -0,0 +1,24 @@
+is_learning = True
+while is_learning:
+    print("learning")
+    is_learning = False
+    
+is_learning = True
+while is_learning:
+    print("learning")
+    is_learning = input("are they still learning")
+    
+is_learning = True
+while is_learning:
+    print("You are learning")
+    user_input = input("They are learning")
+    is_learning == user_input == "Yes"
+    
+user_input = input("Do you wish to run the program? (yes/no)")
+
+while user_input == "yes":
+    print("We are running")
+    user_input = input("Do you wish to run the program?(yes/no)")
+    
+print("We stopped running")
+    
