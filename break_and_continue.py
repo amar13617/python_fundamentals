3e045bc4bc22acf9ff9d7893492f068c4ed2bd52
LENOVO <amar13617@gmail.com>
2023-06-09 19:26:06 +0530

    else with loop commit

--- break_and_continue.py
+++ break_and_continue.py
@@ -15,3 +15,13 @@ for car in cars:
     print(f"this is {car}")
     print("shipping new car to production")
 
+cars = ["ok", "ok", "ok", "faulty", "ok", "ok"]
+
+for status in cars:
+    if status == "faulty":
+        print("Stopping the production line!")
+        break
+
+    print(f"This car is {status}.")
+else:
+    print("All cars built successfully. No faulty cars!")
